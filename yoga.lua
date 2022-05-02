8176995563da7722255340ec32ba314fc1a373fa
Mxnsterr <69323204+Mxnsterr@users.noreply.github.com>
2022-05-02 16:31:44 +0200

    Update yoga.lua

--- yoga.lua
+++ yoga.lua
@@ -10,48 +10,47 @@ exports['qb-target']:AddTargetModel({-1978741854, 2057317573, -232023078}, { --Y
     distance = 2.5
 })
 
-local SucceededAttempts = 0
-local NeededAttempts = 2
+local succeededAttempts = 0
+local neededAttempts = 2
 
 RegisterNetEvent('yoga:start', function()
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
-    local Skillbar = exports['qb-skillbar']:GetSkillbarObject()
+    local skillbar = exports['qb-skillbar']:GetSkillbarObject()
 
     TriggerEvent('animations:client:EmoteCommandStart', {"yoga"})
     FreezeEntityPosition(ped, true)
 
-    Skillbar.Start({
+    skillbar.Start({
         duration = math.random(7500, 15000),
         pos = math.random(10, 30),
         width = math.random(10, 20),
     }, function()
-        if SucceededAttempts + 1 >= NeededAttempts then
+        if succeededAttempts + 1 >= neededAttempts then
             ClearPedTasks(ped)
-            SucceededAttempts = 0
+            succeededAttempts = 0
             FreezeEntityPosition(ped, false)
             TriggerServerEvent('hud:server:RelieveStress', 5)
             QBCore.Functions.Notify("You have a bit less stress", "success")
         else
-            Skillbar.Repeat({
+            skillbar.Repeat({
                 duration = math.random(700, 1250),
                 pos = math.random(10, 40),
                 width = math.random(10, 13),
             })
-            SucceededAttempts = SucceededAttempts + 1
+            succeededAttempts = succeededAttempts + 1
         end
     end, function()
         ClearPedTasks(ped)
         FreezeEntityPosition(ped, false)
         QBCore.Functions.Notify("Failed, focus..", "error")
         SetPedToRagdollWithFall(ped, 1000, 2000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        SucceededAttempts = 0
+        succeededAttempts = 0
     end)
 end)
 
-Citizen.CreateThread(function()
-    local coords = vector3(-1493.59, 829.14, 181.6)
-    local blip = AddBlipForCoord(coords)
+CreateThread(function()
+    local blip = AddBlipForCoord(-1493.59, 829.14, 181.6)
     SetBlipSprite(blip, 197)
     SetBlipDisplay(blip, 4)
     SetBlipScale(blip, 0.6)
